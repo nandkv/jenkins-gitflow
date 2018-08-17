cab978b1ffe5f08baad548d11ac5b2728eb493bf
nand <nandkv.online@gmail.com>
2018-08-16 22:07:21 -0500

    Iniital commit

---
+++ src/main/java/com/example/jenkinsgitflow/JenkinsGitflowApplication.java
@@ -0,0 +1,12 @@
+package com.example.jenkinsgitflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsGitflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsGitflowApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/jenkinsgitflow/JenkinsGitflowApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.jenkinsgitflow;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JenkinsGitflowApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
